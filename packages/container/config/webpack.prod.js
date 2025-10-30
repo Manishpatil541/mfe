3ce8cd59a16f6b747e090b65a22b989fec275fa4
Manishpatil541 <manishpatil1036@gmail.com>
2025-10-30 20:34:38 +0530

    inital commit

---
+++ packages/container/config/webpack.prod.js
@@ -0,0 +1,22 @@
+const { marge } = require('webpack-merge');
+const ModuleFederationPlugin = require('webpack/lib/container/ModuleFederationPlugin');
+const commonConfig = require('./webpack.common');
+const packageJson = require('../../package.json');
+
+const domain = process.env.PRODUCTION_DOMAIN;
+
+const prodConfig = {
+    mode: 'production',
+    output: {
+        filename: '[name].[contenthash].js',
+        // publicPath: 'https://marketing.example.com/',
+    },
+    plugins: [
+        new ModuleFederationPlugin({
+            name: 'container',
+            remotes: {
+                marketing: 'marketing@https://marketing.example.com/remoteEntry.js',
+            }
+        })
+    ]
+}
